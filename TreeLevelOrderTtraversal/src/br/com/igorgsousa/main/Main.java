397d09868b44aab33e64c0ed3f177e97cfc74315
igor guilherme <igorguilherme.s555@gmail.com>
2020-04-05 19:37:40 -0300

    Finalizado projeto "tree-level-order-traversal"

---
+++ TreeLevelOrderTtraversal/src/br/com/igorgsousa/main/Main.java
@@ -0,0 +1,65 @@
+package br.com.igorgsousa.main;
+
+
+import java.util.Scanner;
+
+/**
+ * {@link https://www.hackerrank.com/challenges/tree-level-order-traversal/problem}
+ * */
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+public class Main {
+
+	
+	public static void levelOrder(Node root) {
+	      
+		 System.out.print(root.data+" ");
+
+		 if(root.left != null) {
+	    	  levelOrder(root.left);
+	     }
+		 
+		 if(root.right != null) {
+	    	  levelOrder(root.right);
+	     }	
+    }
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+	 public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
+	        int t = scan.nextInt();
+	        Node root = null;
+	        while(t-- > 0) {
+	            int data = scan.nextInt();
+	            root = insert(root, data);
+	        }
+	        scan.close();
+	        levelOrder(root);
+    }	
+}
+
